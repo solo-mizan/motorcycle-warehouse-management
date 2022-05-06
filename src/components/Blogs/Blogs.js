1682d0ab09b4229f56e3ace36e5f4eb41fc9a33d
solo-mizan <mizanbd.eee@gmail.com>
2022-05-07 00:52:56 +0600

    footer added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,8 +3,17 @@ import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div>
-            <p>Blog question and answer here</p>
+        <div className='text-center'>
+            <h2>Difference between javascript and nodejs</h2>
+            <p></p>
+            <br />
+            <h2>When should you use nodejs and when should you use mongodb?</h2>
+            <p></p>
+            <br />
+            <h2>Differences between sql and nosql databases</h2>
+            <p></p>
+            <br />
+            <h2>What is the purpose of jwt and how does it work?</h2>
         </div>
     );
 };
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,85 @@
 import React from 'react';
 import './Footer.css';
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBIcon
+} from 'mdb-react-ui-kit';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer area</h3>
-        </div>
+        <MDBFooter className='text-center text-white footer-div'>
+            <MDBContainer className='pt-4'>
+                <section className='mb-4'>
+                    <a
+                        className='btn btn-link btn-floating btn-lg text-dark m-1'
+                        href='https://facebook.com/solo-mizan.7/'
+                        target='_blank'
+                        rel='noreferrer'
+                        role='button'
+                        data-mdb-ripple-color='dark'
+                    >
+                        <MDBIcon fab className='fab fa-facebook-f' />
+                    </a>
+
+                    <a
+                        className='btn btn-link btn-floating btn-lg text-dark m-1 disabled'
+                        href='#!'
+                        role='button'
+                        data-mdb-ripple-color='dark'
+                    >
+                        <MDBIcon fab className='fa-twitter' />
+                    </a>
+
+                    <a
+                        className='btn btn-link btn-floating btn-lg text-dark m-1 disabled'
+                        href='#!'
+                        role='button'
+                        data-mdb-ripple-color='dark'
+                    >
+                        <MDBIcon fab className='fa-google' />
+                    </a>
+
+                    <a
+                        className='btn btn-link btn-floating btn-lg text-dark m-1 disabled'
+                        href='#!'
+                        role='button'
+                        data-mdb-ripple-color='dark'
+                    >
+                        <MDBIcon fab className='fa-instagram' />
+                    </a>
+
+                    <a
+                        className='btn btn-link btn-floating btn-lg text-dark m-1 disabled'
+                        href='#!'
+                        role='button'
+                        data-mdb-ripple-color='dark'
+                    >
+                        <MDBIcon fab className='fa-linkedin' />
+                    </a>
+
+                    <a
+                        className='btn btn-link btn-floating btn-lg text-dark m-1'
+                        href='https://github.com/solo-mizan/'
+                        target='_blank'
+                        rel='noreferrer'
+                        role='button'
+                        data-mdb-ripple-color='dark'
+                    >
+                        <MDBIcon fab className='fa-github' />
+                    </a>
+                </section>
+            </MDBContainer>
+
+            <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                © 2022<span> - </span>
+                <a className='text-dark' href='https://github.com/solo.mizan'>
+                    Warehouse Management Service
+                </a>
+            </div>
+        </MDBFooter>
     );
 };
 
